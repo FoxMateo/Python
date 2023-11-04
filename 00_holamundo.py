7ac16a22a34fa0bce325335fdaa17a631d4171d8
FoxMateo <oscarmateobodas@gmail.com>
2023-11-04 18:51:15 +0100

    Prueba Code

---
+++ 00_holamundo.py
@@ -0,0 +1,15 @@
+#Esto es un comentario
+"""
+Esto tambien es un comentario
+en varias lineas
+"""
+
+# Podemos poner la cadena de texto con "" o ''
+# Print saca por pantalla
+print ("Hola python") 
+
+# Ponsulta el tipo de dato
+print (type("soy un dato str")) # tipo str
+print (type(5)) #tipo int
+print (type(5.5)) #tipo float
+print (type(True)) #tipo bool
