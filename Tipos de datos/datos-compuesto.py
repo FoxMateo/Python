8271a855430e1b05b61db6a9e5393d320cf0a583
FoxMateo <oscarmateobodas@gmail.com>
2023-11-05 16:56:42 +0100

    1h de video

---
+++ Tipos de datos/datos-compuesto.py
@@ -0,0 +1,7 @@
+# Son datos que dentro de ellos tienen otros datos
+
+# lista con diferentes datos
+lista = ["oscar mateo", "cositas", True, 1.80]
+print(lista)
+# Nos saca por pantalla el dato 3 de la lista, ya que se empieza a contar desde el 0
+print(lista[2])
---
+++ Variables/variables.py
@@ -0,0 +1,19 @@
+# Variables
+# las variables se declaran y se definen, podemos redefinir variables
+
+numero = 10
+numero = 20
+print(numero)
+
+# Muestra el contenido de la variable {nombre}
+# Con la f todos los datos que pongamos salen en formato string
+nombre = "Jose"
+bienvenida = f"Hola {nombre} como estas?"
+# borra la variable nombre
+del nombre
+# operadores de pertenencia in / not in
+print("Hola" in bienvenida) 
+
+a = 5
+b = 3
+print(a+b)
